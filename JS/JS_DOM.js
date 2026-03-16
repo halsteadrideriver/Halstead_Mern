40a014b34ed066a45e49c21f5b48241e43daf05b
halsteadrideriver <halstead@rideriver.com>
2026-03-16 22:09:22 +0530

    DOM started.

---
+++ JS/JS_DOM.js
@@ -0,0 +1,23 @@
+//to get elements by tagnames
+/*
+a = document.getElementsByTagName("li");
+console.log(a);*/
+
+// to get element by class name:
+/*
+a = document.getElementsByClassName("hero");
+console.log(a);*/
+
+// to get element by ID:  gets unique ID 
+
+// a = document.getElementById("yy");
+// console.log(a);
+
+// to get element by queryselector: selects only one element of any config .
+
+// a = document.querySelector(".hero");
+// console.log(a);
+
+//to get data by selectall : selects all the tags or anything.
+// a = document.querySelectorAll("li");
+// console.log(a);
